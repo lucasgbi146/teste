ba7e6a65b6eb7c8a54f5f096efd9132e8ae1fd06
lucasgbi146 <lucasgbi146@gmail.com>
2019-10-10 12:56:35 -0300

    aula banco de dados

--- meuApp.py
+++ meuApp.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
 
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "pagina inicial"
+    return render_template('index.html')
+
+
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    identificador = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(40), unique=True, nullable=False)
+    senha = db.Column(db.String(12), nullable=False)
+    email = db.Column(db.String(180), unique=True, nullable=False)
+    
+    def __repr__(self): #metedo
+        return "<User %r>" % self.usuario
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
